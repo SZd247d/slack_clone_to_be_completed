ba7bcb5e68dbdcea40881cc4fe3cba0b2bbb705f
zakisellali <73850509+zakisellali@users.noreply.github.com>
2022-03-07 20:50:52 +0100

    Update test.js

--- components/test.js
+++ components/test.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 function test() {
-  return <div>testing mergin conflicts</div>
+  return <div>testing mergin conflicts
+    <h3 className="pl-2 pr-6>#Zaki</h3>
+    </div>
 }
 
 export default test
