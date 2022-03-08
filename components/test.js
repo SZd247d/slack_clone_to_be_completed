1ce2ed4dabdd787787d5cba53bf41b03ab67fdeb
zakisellali <zakisellali464@gmail.com>
2022-03-08 08:57:38 +0100

    resolved

--- components/test.js
+++ components/test.js
@@ -3,11 +3,7 @@ import React from 'react'
 function test() {
   return (
     <div>
-<<<<<<< HEAD
-      <h3>hello zaki sellali</h3>
-=======
       <h3>hello zaki</h3>
->>>>>>> fee7804d38ae98c8fdeab18385ea08bb57490d5b
     </div>
   )
 }
